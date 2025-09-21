904bb4ab2700a585c782a72019326f9b0be4f8e0
timlock <tim-lock@hotmail.de>
2025-09-21 15:26:07 +0200

    add FlagSet WIP

---
+++ README.md
@@ -0,0 +1,7 @@
+## Flags
+
+Library for Go like flags
+
+**References**
+- https://pkg.go.dev/flag
+- https://nullprogram.com/blog/2020/08/01/
--- src/root.zig
+++ src/root.zig
@@ -24,7 +24,7 @@ pub fn Flag(comptime T: type) type {
             };
         }
 
-        pub fn set(self: *Self, value: []const u8) !void {
+        pub fn set(self: *Self, value: []const u8) Error!void {
             if (T == []const u8 or T == []u8) {
                 self.value = value;
             } else switch (@typeInfo(T)) {
@@ -37,20 +37,14 @@ pub fn Flag(comptime T: type) type {
                         return Error.ParseBoolError;
                     }
                 },
-                .int => {
-                    self.value = std.fmt.parseInt(T, value, 10) catch return Error.ParseIntError;
-                },
-                .float => {
-                    self.value = std.fmt.parseFloat(T, value) catch return Error.ParseFloatError;
-                },
+                .int => self.value = std.fmt.parseInt(T, value, 10) catch return Error.ParseIntError,
+                .float => self.value = std.fmt.parseFloat(T, value) catch return Error.ParseFloatError,
                 .@"enum" => {
                     if (std.meta.stringToEnum(T, value)) |enum_field| {
                         self.value = enum_field;
                     } else return Error.ParseEnumError;
                 },
-                else => {
-                    @compileError("Unsupported type for Flag.parse: " ++ @typeName(T));
-                },
+                else => @compileError("Unsupported type for Flag.parse: " ++ @typeName(T)),
             }
 
             self.activated = true;
@@ -63,6 +57,10 @@ pub fn Flag(comptime T: type) type {
 
             self.value = true;
             self.activated = true;
+        }
+
+        pub fn setter(self: *Self) Setter {
+            return Setter.init(self);
         }
     };
 }
@@ -196,3 +194,84 @@ test "parse enum error" {
     try std.testing.expectError(Error.ParseEnumError, enum_flag.set("no enum value"));
     try std.testing.expect(!enum_flag.activated);
 }
+
+const Setter = struct {
+    ptr: *anyopaque,
+    setFn: *const fn (self: *anyopaque, value: []const u8) Error!void,
+    activateFn: *const fn (self: *anyopaque) Error!void,
+
+    fn init(ptr: anytype) Setter {
+        const T = @TypeOf(ptr);
+        const ptr_info = @typeInfo(T);
+
+        const gen = struct {
+            pub fn set(pointer: *anyopaque, value: []const u8) Error!void {
+                const self: T = @ptrCast(@alignCast(pointer));
+                return ptr_info.pointer.child.set(self, value);
+            }
+
+            pub fn activate(pointer: *anyopaque) Error!void {
+                const self: T = @ptrCast(@alignCast(pointer));
+                return ptr_info.pointer.child.activate(self);
+            }
+        };
+
+        return .{
+            .ptr = ptr,
+            .setFn = gen.set,
+            .activateFn = gen.activate,
+        };
+    }
+
+    pub fn set(self: Setter, value: []const u8) Error!void {
+        return self.setFn(self.ptr, value);
+    }
+
+    pub fn activate(self: Setter) Error!void {
+        return self.activateFn(self.ptr);
+    }
+};
+
+test "set bool flag setter" {
+    const BoolFlag = Flag(bool);
+
+    var bool_flag = BoolFlag.init(false, "bool", 'b');
+    try std.testing.expectEqual(false, bool_flag.value);
+    try std.testing.expect(!bool_flag.activated);
+
+    var setter = bool_flag.setter();
+
+    try setter.set("true");
+    try std.testing.expectEqual(true, bool_flag.value);
+    try std.testing.expect(bool_flag.activated);
+}
+
+const BoolFlag = Flag(bool);
+
+pub const FlagSet = struct {
+    name: []const u8,
+    long_map: std.StringArrayHashMap(*Setter),
+    short_map: std.AutoArrayHashMap(u8, []const u8),
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator, name: []const u8) FlagSet {
+        return .{
+            .name = name,
+            .long_map = std.StringArrayHashMap(*Setter).init(allocator),
+            .short_map = std.AutoArrayHashMap(u8, []const u8).init(allocator),
+            .allocator = allocator,
+        };
+    }
+
+    pub fn Bool(self: *FlagSet, name: []const u8, short: u8, default: bool) !*bool {
+        var flag = BoolFlag.init(default, name, short);
+        var setter //TODO wrap flag in setter
+        try self.short_map.put(short, name);
+        try self.long_map.put(name, flag);
+    }
+
+    pub fn deinit(self: *FlagSet) FlagSet {
+        self.long_map.deinit();
+        self.short_map.deinit();
+    }
+};
